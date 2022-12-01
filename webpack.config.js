7f14c572b0bb3a1bf3b60b758389a736cd08236e
unknown <greg@mycunningham.com>
2022-11-30 22:28:45 -0600

    submit for help3

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-export default {
-  devtool: "inline-source-map",
+module.exports = {
+  devtool: "source-map",
   entry: {
     main: "./src/pages/index.js",
   },
@@ -13,16 +13,16 @@ export default {
     filename: "main.js",
     publicPath: "",
   },
-  target: ["web", "es5"],
-  stats: "errors-only",
+  //target: ["web", "es5"],
+  //stats: "errors-only",
   mode: "development",
   devServer: {
-    static: path.resolve(__dirname, "./dist"),
+    contentBase: path.resolve(__dirname, "./dist"),
     compress: true,
     port: 8080,
     open: true,
     liveReload: true,
-    hot: false,
+    //hot: false,
   },
   module: {
     rules: [
@@ -33,7 +33,7 @@ export default {
         test: /\.js$/,
         // all files must be processed by babel-loader
         loader: "babel-loader",
-        // exclude the node_modules folder, we don’t need to process files in it
+        // exclude the node_modules folder, we don't need to process files in it
         exclude: "/node_modules/",
       },
       {
@@ -50,9 +50,18 @@ export default {
         ],
       },
       {
-        // add the rule for processing files
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
         type: "asset/resource",
+        generator: {
+          filename: "images/[name].[hash][ext]",
+        },
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name].[hash][ext]",
+        },
       },
     ],
   },
