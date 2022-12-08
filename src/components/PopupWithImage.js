7e20c787986d40d493990ddce883ce60bdaef603
unknown <greg@mycunningham.com>
2022-12-07 21:51:08 -0600

    changes on the pull info for card2

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -2,10 +2,11 @@ import Popup from "./Popup.js";
 
 class PopupWithImage extends Popup {
   open({ link, name }) {
-    this._popupElement.querySelector(".popup__content").textContent = name;
-    const image = this._popupElement.querySelector(".card__image");
+    this._popupElement.querySelector(".popup__preview-caption").textContent =
+      name;
+    const image = this._popupElement.querySelector(".popup__image");
     image.src = link;
-    image.alt = "${name}";
+    image.alt = name;
 
     super.open();
   }
